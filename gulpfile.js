03def9eb6bd999dd80b26d667cc4e289c56a08bc
dantew359 <dantew2@inbox.ru>
2018-09-29 18:51:21 +0300

    dantew-scss v.0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('css-libs', ['sass'], function() {
 });
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+	gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
 	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
